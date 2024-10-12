9fa116ec29ea56bdbc4d1a1d1e07cceab00e2fb2
Dead0utside <volodya1234321@gmail.com>
2024-10-12 22:39:26 +0200

    complete benefits section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
-	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+	const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+		SelectedPage.Home,
+	);
 	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
 	useEffect(() => {
@@ -23,8 +26,13 @@ function App() {
 
 	return (
 		<div className="app bg-gray-20">
-			<Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+			<Navbar
+				isTopOfPage={isTopOfPage}
+				selectedPage={selectedPage}
+				setSelectedPage={setSelectedPage}
+			/>
 			<Home setSelectedPage={setSelectedPage} />
+			<Benefits setSelectedPage={setSelectedPage} />
 		</div>
 	);
 }
