22a58595a60ea2f38cc05790bd1a8bb1c282f80f
Dead0utside <volodya1234321@gmail.com>
2024-10-12 16:39:29 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useState } from "react"
-import Navbar from "./scenes/navbar"
+import { useEffect, useState } from "react";
+import Navbar from "@/scenes/navbar";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+  });
 
   return (
-    <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}></Navbar>
+    <div className="app bg-gray-20">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      ></Navbar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
